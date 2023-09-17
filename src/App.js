a8372f0b069c29a596de673302e0e3ba388799d7
luan.araujo@watch.tv.br <luan.araujo@watch.tv.br>
2023-09-17 14:37:57 -0300

    adding use state & UltimosLancamentos

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@
 import Header from './componentes/Header'
 import styled from 'styled-components'
 import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos'
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%);
-
   }
   `
 
 function App() {
   return (
     <AppContainer>
-      <Header/>
+<Header/>
       <Pesquisa/>
+      <UltimosLancamentos/>
     </AppContainer>
   );
 }
--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -4,9 +4,9 @@ import IconesHeader from '../IconesHeader'
 import styled from  'styled-components'
 
 const HeaderContainer = styled.header`
-  box-shadow: 0 0 1em black;
   background-color: #fff;
   display: flex;
+  box-shadow: 0 0 1em black;
   justify-content: center;
 `
 function header(){
